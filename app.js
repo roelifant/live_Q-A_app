2430135560678acc5a715652d9c4075f86955068
Roel Van Rossen <roelifant@gmail.com>
2017-08-03 19:50:09 +0200

    routes in apparte bestanden gezet

--- app.js
+++ app.js
@@ -4,23 +4,16 @@ const router = express.Router();
 const path = require('path');
 const pug = require('pug');
 
+//routes
+const login = require('./app/routes/login.js');
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(express.static(__dirname + '/public')); //vertelt waar de statische bestanden staan.
 
-router.get('/', function (req, res) { //router voor index pagina
-    //res.send('Hello World!')
-    res.render('index', {});
-})
-
-router.get('/login', function (req, res) { //router voor "login" pagina
-    res.render('login', {});
-})
-
-router.get('/signin', function (req, res) { //router voor "signin" pagina
-    res.render('signin', {});
-})
+app.use("/", require('./app/routes/index'));
+app.use("/login", login);
 
 app.use('/', router);
 
